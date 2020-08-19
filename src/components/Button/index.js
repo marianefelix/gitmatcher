37dba74fdfca5832a480d7067f3f79e59ce9fd42
marianefelix <marianefelix59@gmail.com>
2020-08-19 15:35:18 -0300

    Estilização do button verificar

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './styles.css';
+
 function Button({ buttonState, children, onClick }){
     return(
-        <button 
-            type="submit"
+        <div 
             className={ buttonState ? "show-button" : "hide-button" }
-            onClick={onClick}
         >
-            {children}
-        </button>
+            <button type="submit" onClick={onClick}>
+                {children}
+            </button>
+        </div>
+        
     );
 }
 
